0dcb9003e3f4f854105e2613353b238a21f5422a
vishhaldawane <vishalviniing@gmail.com>
2021-07-29 10:35:23 +0530

    This is my first commit

---
+++ First.java
@@ -0,0 +1,8 @@
+class First
+{
+  public static void main(String args[])
+  {
+	System.out.println("Hello1 First Git");
+	System.out.println("Hello2 First Git");
+  }
+}
---
+++ Second.java
@@ -0,0 +1,7 @@
+class Second
+{
+  public static void main(String args[])
+  {
+	System.out.println("Hello Second Git");
+  }
+}
